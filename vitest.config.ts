6026b07508a55843ae51359b6a89fcc45c52b9f5
David Eastmond <davebuck1981@gmail.com>
2026-01-09 13:59:13 -0500

    Refactor vitest configuration to improve path resolution and include patterns

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from "vitest/config";
 import { defineVitestProject } from "@nuxt/test-utils/config";
+import * as path from "path";
+import { defineConfig } from "vitest/config";
+const r = (p: string) => path.resolve(__dirname, p);
 
 export default defineConfig({
   test: {
@@ -7,14 +9,20 @@ export default defineConfig({
       {
         test: {
           name: "unit",
-          include: ["test/{e2e,unit}/*.{test,spec}.ts"],
+          include: ["test/{e2e,unit}/**/*.{test,spec}.ts"],
           environment: "node",
+        },
+        resolve: {
+          alias: {
+            "~~/shared": r("./shared"),
+            "~": r("./server"),
+          },
         },
       },
       await defineVitestProject({
         test: {
           name: "nuxt",
-          include: ["test/nuxt/*.{test,spec}.ts"],
+          include: ["test/nuxt/**/*.{test,spec}.ts"],
           environment: "nuxt",
         },
       }),
